9d611d420721e0d0169c0b7a63b8fce24d1bedfc
Josh Datko <jbdatko@gmail.com>
2014-12-02 14:26:36 -0700

    Fixed license header.

--- src/CryptoBuffer.h
+++ src/CryptoBuffer.h
@@ -1,20 +1,20 @@
 /* -*- mode: c++; c-file-style: "gnu" -*-
  * Copyright (C) 2014 Cryptotronix, LLC.
  *
- * This file is part of atsha204-arduino.
+ * This file is part of cryptoauth-arduino.
  *
- * atsha204-arduino is free software: you can redistribute it and/or modify
+ * cryptoauth-arduino is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * atsha204-arduino is distributed in the hope that it will be useful,
+ * cryptoauth-arduino is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with atsha204-arduino.  If not, see <http://www.gnu.org/licenses/>.
+ * along with cryptoauth-arduino.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #ifndef LIB_CRYPTOBUFFER_H_
